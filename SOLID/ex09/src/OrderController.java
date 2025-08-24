a4cf27dc76c7d1d072ffec24e83d79f0bd0e1622
TheHero29 <loukik.23bcs10059@ms.sst.scaler.com>
2025-08-24 14:11:24 +0530

    ex09 done

--- SOLID/ex09/src/OrderController.java
+++ SOLID/ex09/src/OrderController.java
@@ -1,6 +1,9 @@
 public class OrderController {
+    OrderRepository repo;
+    OrderController(OrderRepository repo){
+        this.repo = repo;
+    }
     void create(String id){
-        SqlOrderRepository repo = new SqlOrderRepository(); // hard dependency
         repo.save(id);
         System.out.println("Created order: " + id);
     }
